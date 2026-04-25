c2ff1c6bec392f0933c9ae3b002e37d39750ac67
abby <abigailrodrigo30@gmail.com>
2026-04-25 10:09:15 +0800

    chore : updated the util folder and remove some of the class

--- StatusEffectable.java
+++
@@ -1,8 +0,0 @@
-// Interface for entities that can have status effects applied
-public interface StatusEffectable {
-    boolean isStunned();
-    void applyStun(int turns);
-    void applyTaunt(int turns);
-    void applyDamageReduction(double percent, int turns);
-    void tickStatus();
-}
